f2dc5120ecde311e1f8dcf4d06fb865b7baf0d1e
cptCrunchy <jasong84@gmail.com>
2017-05-18 19:48:42 -0500

    Moved over original files : QuestionForms, QuestionList, Socket, VoteButton into data/models dir

---
+++ src/js/pages/Rooms.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Article from "../components/Article";
+// import QuestionList from "../components/QuestionList";
+// import QuestionForm from "../components/QuestionForm";
+
+export default class Rooms extends React.Component {
+  render() {
+    return (
+      <div>
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="well text-center">
+
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+        
+        </div>
+      </div>
+    );
+  }
+}
